27b338d9fc2cfc3a026a2015f4b1ff21630f9023
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-24 19:54:01 +0100

    feat: Render the google map

---
+++ src/MapContainer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends React.Component {
+  render() {
+    const style = {
+      width: '70vw',
+      height: '100vh'
+    }
+
+    return (
+      <div id='map' role="application">
+        <Map
+          google={this.props.google}
+          initialCenter={{
+            lat: 54.58237,
+            lng: -0.97367
+          }}
+          zoom={15}
+          style = { style }
+        ></Map>
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyC7CpjmKGXh5uri0CQ2h5wad2l5dWteiJE'
+})(MapContainer);
