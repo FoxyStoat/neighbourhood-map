74485da119da0ea56bbfdb4c50ab8e1985f7dcdc
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-25 15:49:44 +0100

    feat: Add location filter component

---
+++ src/LocationFilter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LocationFilter extends React.Component {
+  render() {
+    return (
+      <div className="location-filter-container">
+
+      </div>
+    )
+  }
+}
+
+export default LocationFilter;
