dd91f45ca4e0e64301637169deee0df3b6fee52b
FoxyStoat <carol.hodgson@hotmail.com>
2018-07-25 14:40:08 +0100

    feat: Add map type hybrid

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends React.Component {
   render() {
     const style = {
       width: '100vw',
       height: '60vh',
+      position: 'relative'
     }
 
     return (
-      <div id="map-container" role="application" style = {style}>
+      <div id="map-container" role="application" style={style}>
         <Map
           google={this.props.google}
           initialCenter={{
@@ -18,6 +19,7 @@ export class MapContainer extends React.Component {
           }}
           zoom={15}
           style = {style}
+          mapType = 'hybrid'
         ></Map>
       </div>
     );
