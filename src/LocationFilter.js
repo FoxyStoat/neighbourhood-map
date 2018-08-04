c3be45df3adc4e444c35d46b679b6d5c20a6d06e
FoxyStoat <carol.hodgson@hotmail.com>
2018-08-04 09:33:09 +0100

    feat: Refactor location filter to be a stateless functional component

--- src/LocationFilter.js
+++ src/LocationFilter.js
@@ -1,29 +1,26 @@
 import React from 'react';
 
-class LocationFilter extends React.Component {
+const LocationFilter = (props) => {
 
-  render() {
-    const { query, updateQuery } = this.props;
-
-    return (
-      <div className="location-filter-container">
-        <h2>Locations List</h2>
-        {JSON.stringify(query)}
-        <form
-          role="search">
-          <input
-            role="search"
-            aria-label="search text"
-            className="location-search"
-            type="text"
-            placeholder="Filter Locations..."
-            value={query} //value will always be whatever state query is
-            onChange={(event) => updateQuery(event.target.value)} //when input field changes, update our query
-          />
-        </form>
-      </div>
-    )
-  }
+  const { query, updateQuery } = props;
+  return (
+    <div className="location-filter-container">
+      <h2>Locations List</h2>
+      {/* {JSON.stringify(query)} */}
+      <form
+        role="search">
+        <input
+          role="search"
+          aria-label="search text"
+          className="location-search"
+          type="text"
+          placeholder="Filter Locations..."
+          value={query} //value will always be whatever state query is
+          onChange={(event) => updateQuery(event.target.value)} //when input field changes, update our query
+        />
+      </form>
+    </div>
+  )
 }
 
 export default LocationFilter;
