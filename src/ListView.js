837a788f23164f9ee7c9850adcaf873f63a8704c
FoxyStoat <carol.hodgson@hotmail.com>
2018-08-08 09:02:40 +0100

    feat: Add tab index and role to list view items for focus a11y

--- src/ListView.js
+++ src/ListView.js
@@ -19,6 +19,8 @@ class ListView extends React.Component {
           <ul role="navigation" className="location-list">
             {showingLocations.map((location) => (
               <li
+                tabindex='0'
+                role='button'
                 key={location.id}
                 onClick={event => locationItemClick(location)} // On location click in the listview
                 className="location">
